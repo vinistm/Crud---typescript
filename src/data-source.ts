ff9fa6bf018477310ca497486fad82c29ef6e5be
Vinicius <vinicius.stmartins@gmail.com>
2022-09-20 13:59:14 -0300

    crud completo
    Signed-off-by: Vinicius <vinicius.stmartins@gmail.com>

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,23 +1,54 @@
 
 import { DataSource } from "typeorm";
-import "dotenv/config";
-export const AppDataSource =
-  process.env.NODE_ENV === "test"
-    ? new DataSource({
-        type: "sqlite",
-        database: ":memory:",
-        entities: ["src/entities/*.ts"],
-        synchronize: true,
-      })
-    : new DataSource({
+    require('dotenv').config()
+    
+    export const AppDataSource = new DataSource({
         type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
-        logging: true,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === "production" ?
+            { rejectUnauthorized: false}
+            : false,
         synchronize: false,
-        entities: ["src/entities/*.ts"],
-        migrations: ["src/migrations/*.ts"],
-      });
+        logging: true,
+        entities: process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations: process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
+    })
+    
+    
+    
+    AppDataSource.initialize()
+        .then(() => {
+            console.log("Data Source initialized")
+        })
+        .catch((err) => {
+            console.error("Error during Data Source initialization", err)
+        })
+        
+
+//insomnia
+// import { DataSource } from "typeorm";
+// import "dotenv/config";
+// export const AppDataSource =
+//   process.env.NODE_ENV === "test"
+//     ? new DataSource({
+//         type: "sqlite",
+//         database: ":memory:",
+//         entities: ["src/entities/*.ts"],
+//         synchronize: true,
+//       })
+//     : new DataSource({
+//         type: "postgres",
+//         host: process.env.POSTGRES_HOST,
+//         port: 5432,
+//         username: process.env.POSTGRES_USER,
+//         password: process.env.POSTGRES_PASSWORD,
+//         database: process.env.POSTGRES_DB,
+//         logging: true,
+//         synchronize: false,
+//         entities: ["src/entities/*.ts"],
+//         migrations: ["src/migrations/*.ts"],
+//       });
